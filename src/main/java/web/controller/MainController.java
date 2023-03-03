66668061e37e7f7416ef9df5ca21df907a6bd2a2
Maslovsky-dev <maslovskyvn.nano@ya.ru>
2023-03-03 17:59:06 +0300

    Realize add button for users

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -2,8 +2,11 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import web.model.User;
 import web.service.UserService;
 
@@ -16,10 +19,23 @@ public class MainController {
 	UserService userService;
 
 	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
+	public String printWelcome(Model model) {
 		List<User> allUsers = userService.listUsers();
 		model.addAttribute("allUsers", allUsers);
 		return "main";
+	}
+
+	@GetMapping(value = "/addNewUser")
+	public String addNewUser(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "user-info";
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute ("user") User user) {
+		userService.add(user);
+		return "redirect:/";
 	}
 	
 }
