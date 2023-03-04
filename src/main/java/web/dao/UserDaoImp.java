b4e63dbe9207db0b64aebd09638d85e0d2bf5be4
Maslovsky-dev <maslovskyvn.nano@ya.ru>
2023-03-04 22:44:51 +0300

    Change SessionFactory for EntityManager

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,10 +1,10 @@
 package web.dao;
 
 import web.model.User;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -12,31 +12,30 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+   @PersistenceContext
+   private EntityManager entityManager;
 
    @Override
    public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
+      entityManager.persist(user);
    }
 
-   @Override
-   public User userById(Long id) {
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where id = :id");
-      query.setParameter("id",id);
-      return query.getSingleResult();
-   }
 
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = entityManager.createQuery("from User", User.class);
       return query.getResultList();
    }
-
+   @Override
+   public User userById(Long id) {
+      TypedQuery<User> query = entityManager.createQuery("from User where id = :id", User.class);
+      query.setParameter("id",id);
+      return query.getSingleResult();
+   }
    @Override
    public void update(Long id, User user) {
-      Query query = sessionFactory.getCurrentSession().
+      Query query = entityManager.
               createQuery("update User set firstName = :nameParam, lastName = :lastNameParam," +
               "email = :emailParam where id = :idParam");
       query.setParameter("nameParam",user.getFirstName());
@@ -48,7 +47,7 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public void delete(Long id) {
-      Query query =  sessionFactory.getCurrentSession().
+      Query query =  entityManager.
               createQuery("delete User where id = :idParam");
       query.setParameter("idParam", id);
       int result = query.executeUpdate();
